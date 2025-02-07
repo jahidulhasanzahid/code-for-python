81b613bac7263b4ed2aeea22c5c7bc5c1eea2a40
jahidulhasanzahid <2017zahidhasan@gmail.com>
2025-02-07 17:12:50 -0500

    opptimized

--- main.py
+++
@@ -1,8 +0,0 @@
-# This is a simple python program with an idea. It's help for generate brand name based on your favorite pet and your city name. Here i used python variable declare, user input and string concatenate.
-# this is a welcome message, using python print function
-print("Welcome to your brand name generator.")
-# take two variable for city and pet and take input from user and store value on variable
-cityName = input("Which city you did grow up in? ")
-petName = input("What is your favorite pet name? ")
-# concatenate string and variable
-print("Your brand name could be, "+cityName+" "+petName)
