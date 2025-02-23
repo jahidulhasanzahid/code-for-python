1a9568cb9ea525c893d05a6ace8d74c11e62f2bf
jahidulhasanzahid <2017zahidhasan@gmail.com>
2025-02-22 23:06:05 -0500

    Python Calculator Basic program start

---
+++ calculator.py
@@ -0,0 +1,7 @@
+# Python Calculator
+
+operator = input("Enter an operator (+ - * /): ")
+numberOne = input("Enter the 1st number: ")
+numberTwo = input("Enter the 2nd number: ")
+
+print(int(numberOne) + int(numberTwo))
