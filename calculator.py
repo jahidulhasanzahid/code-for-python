0e74c154970f372e214c1c4a15021fd96f554e0d
jahidulhasanzahid <2017zahidhasan@gmail.com>
2025-02-22 23:20:12 -0500

    Add some code comment

--- calculator.py
+++ calculator.py
@@ -1,9 +1,11 @@
 # Python Calculator
 
+# Taking user input for operator as like + - * /
 operator = input("Enter an operator (+ - * /): ")
+# Taking two user input for number one and number two calculate
 numberOne = float(input("Enter the 1st number: "))
 numberTwo = float(input("Enter the 2nd number: "))
-
+# Checking operator and calculate based on operator
 if operator == "+":
     result = numberOne + numberTwo
     print(round(result))
