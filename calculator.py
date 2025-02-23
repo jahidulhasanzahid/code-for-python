ecbb806a2e600812b7c2a4b79fef25cbc450706f
jahidulhasanzahid <2017zahidhasan@gmail.com>
2025-02-22 23:13:53 -0500

    condition set for invalid calculate operator

--- calculator.py
+++ calculator.py
@@ -6,13 +6,15 @@ numberTwo = float(input("Enter the 2nd number: "))
 
 if operator == "+":
     result = numberOne + numberTwo
-    print(result)
+    print(round(result))
 elif operator == "-":
     result = numberOne - numberTwo
-    print(result)
+    print(round(result))
 elif operator == "*":
     result = numberOne * numberTwo
-    print(result)
+    print(round(result))
 elif operator == "/":
     result = numberOne / numberTwo
-    print(result)
+    print(round(result))
+else:
+    print(f"{operator} is not a valid operator!")
