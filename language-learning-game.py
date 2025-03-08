6899e11ce01aeaebaf2512d8683fc6be602dfcdf
jahidulhasanzahid <2017zahidhasan@gmail.com>
2025-03-08 14:09:27 -0500

    word object added

--- language-learning-game.py
+++ language-learning-game.py
@@ -1,1 +1,10 @@
 # This is a language learning game, It's fun and easy to learn very common word which we use in our daily life.
+spanish_english_translation = [
+    {"spanish" : "el", "english" : "the"},
+    {"spanish": "de", "english": "of"},
+    {"spanish": "que", "english": "of"},
+    {"spanish": "y", "english": "that"},
+    {"spanish": "a", "english": "to"},
+    {"spanish": "en", "english": "in"},
+]
+# you can add more words here, if you want
