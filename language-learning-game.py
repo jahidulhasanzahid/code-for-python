1fccf0895471cb29a021efde289d5bb398306ebb
jahidulhasanzahid <2017zahidhasan@gmail.com>
2025-03-08 14:22:29 -0500

    program code for basic way to learning language with fun

--- language-learning-game.py
+++ language-learning-game.py
@@ -1,5 +1,7 @@
 # This is a language learning game, It's fun and easy to learn very common word which we use in our daily life.
-spanish_english_translation = [
+import random
+
+words = [
     {"spanish" : "el", "english" : "the"},
     {"spanish": "de", "english": "of"},
     {"spanish": "que", "english": "of"},
@@ -8,3 +10,29 @@ spanish_english_translation = [
     {"spanish": "en", "english": "in"},
 ]
 # you can add more words here, if you want
+
+def quiz(words):
+    random.shuffle(words)
+    score = 0
+
+    for word in words:
+        print(f"What is the english translation of `{word['spanish']}`?")
+        answer = input("Your Answer : ").strip().lower()
+        correct_answer = word['english'].lower()
+
+        if answer == correct_answer:
+            score += 1
+        else:
+            print(f"Wrong, The Correct answer is `{correct_answer}`.\n")
+
+    print(f"Quiz completed! Your score is: `{score}/{len(words)}`")
+
+
+
+def main():
+    print("Welcome to the language learning app.")
+    input("Press Enter to start quiz.....")
+    quiz(words)
+
+if __name__ == '__main__':
+    main()
